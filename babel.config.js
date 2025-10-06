657fe36cdf66c2ff666ea6bb7036d9badf3970fc
Amanda Almeida <almeidafr.amanda@gmail.com>
2025-10-06 14:49:59 -0300

    Ajustes finais

--- babel.config.js
+++ babel.config.js
@@ -5,3 +5,4 @@ module.exports = function (api) {
         plugins: ['react-native-reanimated/plugin'], // 👈 ADICIONE ESTA LINHA
     };
 };
+
--- index.js
+++ index.js
@@ -6,3 +6,4 @@ import App from './advanced_diary_app';
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
 registerRootComponent(App);
+
